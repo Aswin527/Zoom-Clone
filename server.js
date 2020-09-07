ad166ba23dcc3b146731acae46202984a1fa339f
Aswin Ramesh <aswinramesh527@gmail.com>
2020-09-07 20:56:37 +0530

    user adding basics with socket.io library

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid');
+const { Socket } = require('dgram');
 app.use(express.static('public'));
 app.set('view engine','ejs')
 
@@ -12,9 +14,16 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/:room',(req,res)=>{
-    res.render('room',{roomId: req.params.room})
+    res.render('room',{roomId : req.params.room})
 })
 
+io.on('connection',socket =>{
+    socket.on('join-room',(roomId)=>{
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected');
+        console.log('joined the room')
+    });
+})
 
 
 
